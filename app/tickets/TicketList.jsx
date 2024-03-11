b3217a6d07725f2ce2a1830a74e8321b63d126cb
Ishmael Chitsonga <65092392+ishi265@users.noreply.github.com>
2024-03-11 10:40:19 +0200

    changes made in code

---
+++ app/tickets/TicketList.jsx
@@ -0,0 +1,27 @@
+async function getTickets() {
+    const res = await fetch('http://localhost:4000/tickets')
+
+    return res.json()
+}
+
+export default async function TicketList() {
+  const tickets = await getTickets()
+  
+  return (
+    <>
+      {tickets.map((tickets) => {
+        <div key={tickets.id} className="card my-5">
+            <h3>{tickets.title}</h3>
+            <p>{tickets.body.slice(0,200)}</p>
+            <div className={`pill ${ticket.priority}`}>
+                {tickets.priority} Priority
+            </div>
+        </div>
+      })}
+      {tickets.length === 0 && (
+        <p className="text-center">There are no tickets</p>
+      )}
+    </>
+
+  )
+}
