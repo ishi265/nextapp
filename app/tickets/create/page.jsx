14781e59a54863fb3705ef4ffe4eb31815e81f4a
Ishmael Chitsonga <65092392+ishi265@users.noreply.github.com>
2024-05-14 16:42:04 +0200

    changes to code

---
+++ app/tickets/create/page.jsx
@@ -0,0 +1,11 @@
+import CreateForm from "./CreateForm";
+import React from 'react'
+
+export default function CreateTicket() {
+  return (
+    <main>
+        <h2 className="text-primary text-center">Add a New Ticket</h2>
+        <CreateForm />
+    </main>
+  )
+}
